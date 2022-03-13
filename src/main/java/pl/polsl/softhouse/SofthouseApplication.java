55ce4e01e7ea575e8f3114f37c18511ce0c17c06
sbotor <botorszym@gmail.com>
2022-03-13 19:02:50 +0100

    feat: add basic Spring Boot project files

---
+++ src/main/java/pl/polsl/softhouse/SofthouseApplication.java
@@ -0,0 +1,12 @@
+package pl.polsl.softhouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SofthouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SofthouseApplication.class, args);
+	}
+}
---
+++ src/test/java/pl/polsl/softhouse/SofthouseApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.polsl.softhouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SofthouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
